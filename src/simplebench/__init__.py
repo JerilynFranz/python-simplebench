174200043882a790aef77a648405be263356c0bf
Jerilyn <jerilyn.franz@gmail.com>
2025-09-05 16:54:03 -0700

    WIP (broken state)

--- src/simplebench/__init__.py
+++ src/simplebench/__init__.py
@@ -2,6 +2,7 @@
 """Simple benchmarking framework."""
 
 from .case import Case
+from .cli import main
 from .constants import (DEFAULT_INTERVAL_SCALE, DEFAULT_INTERVAL_UNIT,
                         DEFAULT_OPS_PER_INTERVAL_UNIT,
                         DEFAULT_OPS_PER_INTERVAL_SCALE,
@@ -13,18 +14,38 @@ from .runners import SimpleRunner
 from .results import Results
 from .iteration import Iteration
 from .session import Session, Verbosity
-from .tasks import ProgressTasks
+from .stats import OperationsPerInterval, OperationTimings, Stats
+from .tasks import RichProgressTasks, RichTask
 from .utils import (sanitize_filename, si_scale_for_smallest, si_scale, si_scale_to_unit, sigfigs)
+from .reporters.interfaces import Reporter
+from .reporters.choices import Choices, Choice, Section, Target, Format
+from .reporters.rich_table import RichTableReporter
+from .reporters.csv import CSVReporter
+from .reporters.graph import GraphReporter
 
 
 __all__ = [
     # Core classes
     'Case',
+    'Choice',
+    'Choices',
+    'CSVReporter',
+    'Format',
+    'GraphReporter',
     'Iteration',
+    'OperationsPerInterval',
+    'OperationTimings',
+    'Reporter',
     'Results',
-    'SimpleRunner',
-    'ProgressTasks',
+    'RichProgressTasks',
+    'RichTableReporter',
+    'RichTask',
+    'Section',
     'Session',
+    'Stats',
+    'SimpleRunner',
+    'Target',
+    'Verbosity',
 
     # Exceptions
     'ErrorTag',
@@ -45,6 +66,7 @@ __all__ = [
     'BASE_OPS_PER_INTERVAL_UNIT',
 
     # Utility functions
+    'main',
     'sanitize_filename',
     'si_scale_for_smallest',
     'si_scale',
