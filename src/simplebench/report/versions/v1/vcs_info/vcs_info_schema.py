ad384679571410ac29ca01fadbc82c24b238edad
Jerilyn <jerilyn.franz@gmail.com>
2025-12-21 02:23:44 -0800

    Documentation regeneration

--- src/simplebench/report/versions/v1/vcs_info/vcs_info_schema.py
+++ src/simplebench/report/versions/v1/vcs_info/vcs_info_schema.py
@@ -1,5 +1,9 @@
 """Schema for JSON VCSInfo v1 validation."""
 # pylint: disable=line-too-long
+from copy import deepcopy
+from json import JSONEncoder
+
+from simplebench.doc_utils import format_docstring, format_json_for_docstring
 from simplebench.report.base import JSONSchema
 
 
@@ -15,19 +19,7 @@ class VCSInfoSchema(JSONSchema):
     ID: str = "https://raw.githubusercontent.com/JerilynFranz/python-simplebench/main/schemas/v1/vcs-info.json"
     """The JSON VCSInfo schema $id value for version 1 reports."""
 
-    @classmethod
-    def as_dict(cls) -> dict[str, object]:
-        """Get the JSON schema as a dictionary.
-
-        It always returns a fresh copy of the schema dictionary to prevent accidental
-        modifications.
-
-        The caller can modify the returned dictionary as needed or cache it for performance.
-
-        Usage:
-            schema_dict = VCSInfoSchema.as_dict()
-        """
-        return {
+    _JSON_SCHEMA_DICT: dict[str, object] = {
             "$schema": "https://json-schema.org/draft/2020-12/schema",
             "$id": "https://raw.githubusercontent.com/JerilynFranz/python-simplebench/main/schemas/v1/vcs-info.json",
             "title": "VCS Info (V1)",
@@ -93,3 +85,47 @@ class VCSInfoSchema(JSONSchema):
             ],
             "additionalProperties": False
         }
+
+    _JSON_SCHEMA_TEXT: str = JSONEncoder(indent=2).encode(_JSON_SCHEMA_DICT)
+    """The JSON schema as a pretty-printed JSON string."""
+
+    _JSON_SCHEMA_NOTE: str = format_json_for_docstring(
+        json_data=_JSON_SCHEMA_TEXT,
+        caption="JSON Schema for ValueBlock V1",
+        intro_text="The JSON schema is as follows:"
+    )
+    """Note containing the JSON schema for docstrings."""
+
+    @classmethod
+    def as_dict(cls) -> dict[str, object]:
+        """Get the JSON schema as a dictionary.
+
+        It always returns a fresh copy of the schema dictionary to prevent accidental
+        modifications.
+
+        The caller can modify the returned dictionary as needed or cache it for performance.
+
+        Usage:
+            schema_dict = VCSInfoSchema.as_dict()
+        """
+        return deepcopy(cls._JSON_SCHEMA_DICT)
+
+    @classmethod
+    @format_docstring(JSON_SCHEMA_NOTE=_JSON_SCHEMA_NOTE)
+    def as_json(cls) -> str:
+        # The `{JSON_SCHEMA_NOTE}` placeholder in the docstring below is
+        # dynamically replaced by the `@format_docstring` decorator
+        # for use in generated documentation. It is not a typo, but an intentional
+        # use of a placeholder.
+        """Get the JSON schema as a JSON-formatted string.
+
+        It serializes the schema dictionary to a JSON string.
+
+        Usage:
+            schema_json = VCSInfoSchema.as_json()
+
+        :return str: The JSON schema as a JSON-formatted string.
+
+        {JSON_SCHEMA_NOTE}
+        """
+        return cls._JSON_SCHEMA_TEXT
