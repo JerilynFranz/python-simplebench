2c3879dad77ef050f7f240dc6d85657f39c37a67
Jerilyn <jerilyn.franz@gmail.com>
2025-12-23 11:42:11 -0800

    wordsmithing

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -72,10 +72,10 @@ Examples:
     # Add a package to specified extras
     uv add 'package_name[extra1,extra2]'
 
-    # install all dependencies from pyproject.toml
+    # Sync all dependencies from pyproject.toml to the virtual environment
     uv sync --all-extras
 
-    # install a package from PyPI
+    # install a package from PyPI to the virtual environment
     uv pip install 'package_name>=1.2.3'
 
 See https://docs.astral.sh/uv/ for more information on using 'uv'.
